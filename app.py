d4af922896d572b1ae5f9f739c2b3a7ed6e5b322
Cynthia Chen <cdy@cyu.edu.cn>
2025-10-23 13:57:45 +0800

    changed package import

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from langchain_community.document_loaders import PyPDFLoader, Docx2txtLoader, TextLoader
 import tempfile
 import os
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_google_genai import ChatGoogleGenerativeAI
